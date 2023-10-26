8563f9eb052f526aa8663f8964a72e8ba1b04304
Tamás Szabó <tamas@szaboo.com>
2023-10-26 08:50:53 +0200

    initialize repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Info alapok git demo
+
+## Summary
+
+This program implements a basic minimum search algorithm.
+
+The program uses `math.inf` to check the smallest number.
---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+min = math.inf
+
+list = [1, 3, 10, 5, 20, 7, -3, -1]
+temp_smallest = min
+
+for item in list:
+  if item < temp_smallest:
+    temp_smallest = item
+
+print('Minimum', temp_smallest)
