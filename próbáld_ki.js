5aa0a14b4de936b23c50dfbed97da13293ee9d2f
lorantnemeth09 <104296066+lorantnemeth09@users.noreply.github.com>
2023-10-26 09:36:41 +0200

    ima

--- próbáld_ki.js
+++ próbáld_ki.js
@@ -1,1 +1,16 @@
 //┌∩┐(◣_◢)┌∩┐
+
+
+/*Lord Gaben ki vagy a steam mögött,
+Szenteltessék meg a te neved,
+Jöjjön el a te világod,
+Legyen meg a te akaratod,
+Mint a steamen, ugy a CS:GO-ban is.
+A mi mindennapi CS:GO skinünk ad meg nékünk ma
+És VACold a hackereket
+mikép mi is szídjuk a noobokat
+És ne vigyél minket kisértésbe
+De adhatnál néhány drága skint,
+Mert tiéd a steam, a hatalom
+Mindörökké
+Amen*/
